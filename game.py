555c2f94baa35a9394098d6fb3e98aa44130c389
season-longsir <season100201_1@outlook.com>
2026-07-26 21:24:40 +0800

    create game.py and initially init the game

---
+++ game.py
@@ -0,0 +1,34 @@
+# Controll the whole game.
+
+
+import random
+import constants
+from player import Player
+from deck import gnrate_init
+
+
+class Game:
+    def __init__(self, nump, hump):
+        self.n = nump
+        self.players:list[Player] = []
+        for i in range(nump):
+            is_human = (i < hump)
+            p = Player(i, f'P{i+1}', is_human = is_human)
+            p.hand = gnrate_init()
+            self.players.append(p)
+
+        self.secret = random.randint(constants.SECRET_MIN, constants.SECRET_MAX)
+
+        self.round = 0
+        self.curplayer_idx = 0
+
+        self.bomb_active = False
+        self.bombholder_idx = None
+        self.bomb_passes = 0
+        self.pass_limit = 1 if nump == 2 else 2
+
+        self.chal_bdries = set()
+
+    def run(self):
+        # TODO
+        pass
